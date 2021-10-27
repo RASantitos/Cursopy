7cd6dc700a24e3ea512fa9961aa64942660719cc
RASantitos <93273041+RASantitos@users.noreply.github.com>
2021-10-27 10:23:42 -0500

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 09:36:10 2021
+
+@author: sroqu
+"""
+
+variable1=921
+VARIABLE2=89.2378
+Variable3=76-87j
+VaRiAbLe_4="CADENA DE TEXTO"
+#variable5=input("Dígite cualquier cosa: ")
+variable5=4+90+56 #declaración de una variable - en otros lenguajes declara
+                  #cion implica el declarar que tipo de variable es. Aquí no. 
+
+print(type(variable1))
+print(type(VARIABLE2))
+print(type(Variable3))
+print(type(VaRiAbLe_4))
+print(variable5)  
+#print(type(variable5))
